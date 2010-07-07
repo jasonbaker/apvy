403e0254e452417b745fb7023c016216a15e8e73
Jason Baker <amnorvend@gmail.com>
2010-07-07 10:06:39 -0500

    Adding a place for celeryconfig.py

--- apvy/apvyd.py
+++ apvy/apvyd.py
@@ -1,3 +1,8 @@
+import sys
+# Providing a place to put celeryconfig.py so we don't have to export a
+# PYTHONPATH variable which might have unintended side-effects.
+sys.path.append('./pypath')
+
 from twisted.internet import reactor
 from twisted.scheduling.cron import CronSchedule
 from twisted.scheduling.task import ScheduledCall
