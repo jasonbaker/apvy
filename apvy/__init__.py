7e44ca587285f6c56d6a0602889a34cce3de07e2
Jason Baker <amnorvend@gmail.com>
2010-07-07 10:38:59 -0500

    Moving this to __init__.py

--- apvy/__init__.py
+++ apvy/__init__.py
@@ -1,2 +1,7 @@
+import sys
+# Providing a place to put celeryconfig.py so we don't have to export a
+# PYTHONPATH variable which might have unintended side-effects.
+sys.path.append('./pypath')
+
 from apvy.defer import DeferredResult
 from apvy.http import TwistedHttpTask, URL
